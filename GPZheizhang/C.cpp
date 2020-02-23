029d0cd2e2cf94818bb6cd9d3c33f9bfd75c8d47
alexLopatin <minelabory@gmail.com>
2020-02-23 19:18:11 +0300

    GP Zheizhang commit

---
+++ GPZheizhang/C.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+struct Point
+{
+	int x;
+	int y;
+};
+
+long long x_c, y_c, x_0, y_0, r;
+
+unsigned long long CalcSquareDistance(long long x1, long long y1, long long x2, long long y2)
+{
+	return (unsigned long long)(x1 - x2) * (x1 - x2) + (unsigned long long)(y1 - y2) * (y1 - y2);
+}
+
+
+bool IsInCircle(double x, double y)
+{
+	return ((x - x_c) * (x - x_c) + (y - y_c) * (y - y_c) <= r * r);
+}
+
+Point FindClosest(double x, double y)
+{
+	Point result;
+	unsigned long long distance = 18446744073709551615ull;
+	result.x = 0;
+	result.y = 0;
+	long long downBorder = floor(y) - 1420;
+	long long upBorder = ceil(y) + 1420;
+	long long leftBorder = floor(x) - 1420;
+	long long rightBorder = ceil(x) + 1420;
+	for (long long i = leftBorder; i <= rightBorder; i++)
+	{
+		for (long long j = downBorder; j <= upBorder; j++)
+		{
+			bool b = IsInCircle(i, j);
+			if (IsInCircle(i, j) && distance > CalcSquareDistance(x_0, y_0, i, j))
+			{
+				distance = CalcSquareDistance(x_0, y_0, i, j);
+				result.x = i;
+				result.y = j;
+			}
+		}
+	}
+	return result;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++)
+	{
+		cin >> x_c >> y_c >> r >> x_0 >> y_0;
+		double m = (r * r * (double)(y_0 - y_c) * (y_0 - y_c)) / ((x_0 - x_c) * (x_0 - x_c) + (y_0 - y_c) * (y_0 - y_c));
+		if (IsInCircle(x_0, y_0))
+			cout << "0\n" << x_0 << " " << y_0 << endl;
+		else
+		{
+			cout << "1\n" << x_0 << " " << y_0 << " ";
+			double y = sqrt(m) + y_c;
+			double x = sqrt(m) * (double)(x_0 - x_c) / (double)(y_0 - y_c) + x_c;
+			if (x_c == x_0)
+			{
+				y = r + y_c;
+				x = x_c;
+			}
+			if (y_c == y_0)
+			{
+				y = y_c;
+				x = r + x_c;
+			}
+			Point positive = FindClosest(x, y);
+			y = -sqrt(m) + y_c;
+			x = -sqrt(m) * (double)(x_0 - x_c) / (double)(y_0 - y_c) + x_c;
+			if (x_c == x_0)
+			{
+				y = -r + y_c;
+				x = x_c;
+			}
+			if (y_c == y_0)
+			{
+				y = y_c;
+				x = -r + x_c;
+			}
+
+			Point negative = FindClosest(x, y);
+			if (CalcSquareDistance(x_0, y_0, positive.x, positive.y) < CalcSquareDistance(x_0, y_0, negative.x, negative.y))
+				cout << positive.x << " " << positive.y << endl;
+			else
+				cout << negative.x << " " << negative.y << endl;
+		}
+	}
+}
---
+++ GPZheizhang/K.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <math.h>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	int n, m, k;
+	cin >> n >> m >> k;
+	map<int, int> preferences;
+	for (int i = 0; i < n; i++)
+		preferences[i] = 0;
+	for (int i = 0; i < k; i++)
+	{
+		int player, pref;
+		cin >> player >> pref;
+		player--;
+		preferences[player] = preferences[player] | (1 << (pref - 1));
+	}
+	map<int, int> players;
+	for (int j = 0; j < 32; j++)
+	{
+		players[j] = 0;
+		for (int i = 0; i < n; i++)
+		{
+			if ((j & preferences[i]) == 0)
+				players[j]++;
+		}
+	}
+	int res = -1;
+	for (int i = 0; i < 32; i++)
+	{
+		if (players[i] > res&& players[i] <= m)
+			res = players[i];
+	}
+	if (res >= 0)
+		cout << res << endl;
+	else
+		cout << "Round will be unrated\n";
+	return 0;
+}
